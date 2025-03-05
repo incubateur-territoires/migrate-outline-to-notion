10062583369e5c0025d093f69460e9983abf0c50
Mert Çorumlu <mertcorumlu55@gmail.com>
2025-03-05 17:50:23 +0300

    add node types, add readme content, fix try binary block_id (#1)

--- src/bin/try.ts
+++ src/bin/try.ts
@@ -4,7 +4,7 @@ const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
 async function main() {
     const response = await notion.blocks.children.list({
-        block_id: "144aa724-fdce-805d-a123-c60d77af41e5"//process.env.NOTION_DESTINATION_PAGE_ID,
+        block_id: process.env.NOTION_DESTINATION_PAGE_ID,
     });
     console.log(JSON.stringify(response, null, 2));
 }
